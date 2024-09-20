e2aa5cf470acbb64f63551b489ace3d9c696fd78
Maliya14-ux <mr4maliya@gmail.com>
2024-09-21 03:42:03 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "uKhxVAjR#dDfnxFDhQ7TzC46_M2r3oBkx3E3Afw4jqLEtvsYrhUs",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/397000a07a1deb7fad9c2.jpg",
 MONGODB: process.env.MONGODB || "",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
